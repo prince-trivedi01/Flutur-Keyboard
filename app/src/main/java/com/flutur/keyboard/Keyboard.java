8a8c5099a169ae2fe504dbd94ded62c54442dc59
Prince Trivedi <prince-trivedi01@.github.com>
2018-06-27 11:22:03 +0530

    Commit

---
+++ app/src/main/java/com/flutur/keyboard/Keyboard.java
@@ -0,0 +1,109 @@
+package com.flutur.keyboard;
+
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.KeyboardView;
+import android.media.AudioManager;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+
+public class Keyboard extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+
+    private KeyboardView kv;
+    private android.inputmethodservice.Keyboard keyboard;
+
+    private boolean isCaps = false;
+
+
+    //Press Ctrl+O
+
+
+    @Override
+    public View onCreateInputView() {
+        kv = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard, null);
+        keyboard = new android.inputmethodservice.Keyboard(this, R.xml.qwerty);
+        kv.setKeyboard(keyboard);
+        kv.setOnKeyboardActionListener(this);
+        return kv;
+    }
+
+    @Override
+    public void onPress(int i) {
+
+    }
+
+    @Override
+    public void onRelease(int i) {
+
+    }
+
+    @Override
+    public void onKey(int i, int[] ints) {
+
+        InputConnection ic = getCurrentInputConnection();
+        playClick(i);
+        switch (i) {
+            case android.inputmethodservice.Keyboard.KEYCODE_DELETE:
+                ic.deleteSurroundingText(1, 0);
+                break;
+            case android.inputmethodservice.Keyboard.KEYCODE_SHIFT:
+                isCaps = !isCaps;
+                keyboard.setShifted(isCaps);
+                kv.invalidateAllKeys();
+                break;
+            case android.inputmethodservice.Keyboard.KEYCODE_DONE:
+                ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+                break;
+            default:
+                char code = (char) i;
+                if (Character.isLetter(code) && isCaps)
+                    code = Character.toUpperCase(code);
+                ic.commitText(String.valueOf(code), 1);
+        }
+
+    }
+
+    private void playClick(int i) {
+
+        AudioManager am = (AudioManager) getSystemService(AUDIO_SERVICE);
+        switch (i) {
+            case 32:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_SPACEBAR);
+                break;
+            case android.inputmethodservice.Keyboard.KEYCODE_DONE:
+            case 10:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_RETURN);
+                break;
+            case android.inputmethodservice.Keyboard.KEYCODE_DELETE:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_DELETE);
+                break;
+            default:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_STANDARD);
+        }
+    }
+
+    @Override
+    public void onText(CharSequence charSequence) {
+
+    }
+
+    @Override
+    public void swipeLeft() {
+
+    }
+
+    @Override
+    public void swipeRight() {
+
+    }
+
+    @Override
+    public void swipeDown() {
+
+    }
+
+    @Override
+    public void swipeUp() {
+
+    }
+}
